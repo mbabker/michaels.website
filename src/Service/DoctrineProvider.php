ad96ec88967873810b587e750bfa80261671cc33
Michael Babker <mbabker@flbab.com>
2014-12-12 11:00:12 -0500

    Consolidate the Doctrine services to one provider class

--- src/Service/DoctrineProvider.php
+++ src/Service/DoctrineProvider.php
@@ -10,6 +10,11 @@ namespace BabDev\Website\Service;
 
 use BabDev\Website\Doctrine\ManagerRegistry;
 
+use Doctrine\Common\Proxy\AbstractProxyFactory;
+use Doctrine\DBAL\DriverManager;
+use Doctrine\ORM\EntityManager;
+use Doctrine\ORM\Tools\Setup;
+
 use Joomla\DI\Container;
 use Joomla\DI\ServiceProviderInterface;
 
@@ -31,10 +36,54 @@ class DoctrineProvider implements ServiceProviderInterface
 	 */
 	public function register(Container $container)
 	{
-		// Register dependencies
-		$container->registerServiceProvider(new DBALConnectionProvider)
-			->registerServiceProvider(new EntityManagerProvider);
+		// Register the DBAL Connection
+		$container->set('Doctrine\\DBAL\\Connection',
+			function (Container $container)
+			{
+				$config = $container->get('config');
 
+				// Set the DBAL Connection configuration
+				$connection = [
+					'driver'   => $config->get('database.driver'),
+					'host'     => $config->get('database.host'),
+					'user'     => $config->get('database.user'),
+					'password' => $config->get('database.password'),
+					'dbname'   => $config->get('database.name')
+				];
+
+				// Create the Connection
+				return DriverManager::getConnection($connection);
+			}, true, true
+		);
+
+		// Alias the Connection
+		$container->set('doctrine.default.dbal_connection', $container->get('Doctrine\\DBAL\\Connection'));
+
+		// Register the Entity Manager
+		$container->set('Doctrine\\ORM\\EntityManager',
+			function (Container $container)
+			{
+				$config = $container->get('config');
+
+				// Lookup path for entities
+				$paths = [JPATH_ROOT . '/src/Entity'];
+
+				// Flag if we're in dev mode, base this on the database.debug option
+				$devMode = $config->get('database.debug');
+
+				// Setup the configuration
+				$emConfig = Setup::createAnnotationMetadataConfiguration($paths, $devMode);
+				$emConfig->setAutoGenerateProxyClasses(AbstractProxyFactory::AUTOGENERATE_FILE_NOT_EXISTS);
+
+				// Create the EntityManager
+				return EntityManager::create($container->get('doctrine.default.dbal_connection'), $emConfig);
+			}, true, true
+		);
+
+		// Alias the EntityManager
+		$container->alias('doctrine.default.entity_manager', 'Doctrine\\ORM\\EntityManager');
+
+		// Register the Doctrine Manager Registry
 		$container->set('BabDev\\Website\\Doctrine\\ManagerRegistry',
 			function (Container $container)
 			{
