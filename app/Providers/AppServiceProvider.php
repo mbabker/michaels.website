64920fc6619f186b07b7b117662fe8f74591a25a
Michael Babker <michael.babker@gmail.com>
2020-11-26 11:03:15 -0600

    We don't need the page resolver here since the controller is setting the page for us

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -3,7 +3,6 @@
 namespace App\Providers;
 
 use App\Pagination\RoutableLengthAwarePaginator;
-use Illuminate\Pagination\AbstractPaginator;
 use Illuminate\Pagination\LengthAwarePaginator;
 use Illuminate\Support\ServiceProvider;
 
@@ -18,23 +17,6 @@ class AppServiceProvider extends ServiceProvider
     {
         // Bind pagination to our local class
         $this->app->bind(LengthAwarePaginator::class, RoutableLengthAwarePaginator::class);
-
-        // Change the current page resolver to be aware of the route parameters
-        AbstractPaginator::currentPageResolver(function ($pageName = 'page') {
-            $route = $this->app['request']->route();
-
-            if ($page = $route->parameter($pageName)) {
-                return $page;
-            }
-
-            $page = $this->app['request']->input($pageName);
-
-            if (\filter_var($page, \FILTER_VALIDATE_INT) !== false && (int) $page >= 1) {
-                return (int) $page;
-            }
-
-            return 1;
-        });
 
         // Add the route resolver
         RoutableLengthAwarePaginator::currentRouteResolver(function () {
