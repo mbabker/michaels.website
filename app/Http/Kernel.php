64335b2a36302343dfea0c7a384c0e248f145a70
Michael Babker <michael.babker@gmail.com>
2020-11-25 19:49:50 -0600

    We don't have incoming data, or route bindings, so drop the middleware from the request

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -8,14 +8,10 @@ class Kernel extends HttpKernel
 {
     protected $middleware = [
         \Illuminate\Foundation\Http\Middleware\PreventRequestsDuringMaintenance::class,
-        \Illuminate\Foundation\Http\Middleware\ValidatePostSize::class,
-        \Illuminate\Foundation\Http\Middleware\TrimStrings::class,
-        \Illuminate\Foundation\Http\Middleware\ConvertEmptyStringsToNull::class,
     ];
 
     protected $middlewareGroups = [
         'web' => [
-            \Illuminate\Routing\Middleware\SubstituteBindings::class,
             \BabDev\ServerPushManager\Http\Middleware\ServerPush::class,
         ],
     ];
